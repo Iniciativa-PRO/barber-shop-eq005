7e7046c09e51c8aacfceb6ad07ef3571433731fd
Arjios <antonio.uab.ios.fisica@gmail.com>
2023-03-06 16:50:13 -0300

    About Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
 // script da aplicação
+document.getElementById("menu-button").addEventListener("click", menuHamburger);
+
+function menuHamburger() {
+    var x = document.getElementById("links");
+    console.log(x.style.display);
+    if(x.style.display === block) {
+        console.log("BLOCK");
+    } else {
+        console.log("NO BLOCK");
+    }
+}
