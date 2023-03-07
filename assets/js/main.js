85a0fd323905d156abbb60253c552cd320cd5555
Arjios <antonio.uab.ios.fisica@gmail.com>
2023-03-07 17:49:33 -0300

    Menu header responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,9 +4,4 @@ document.getElementById("menu-button").addEventListener("click", menuHamburger);
 function menuHamburger() {
     var x = document.getElementById("links");
     console.log(x.style.display);
-    if(x.style.display === block) {
-        console.log("BLOCK");
-    } else {
-        console.log("NO BLOCK");
-    }
 }
